bd72c0e04502fa8e2670a46292c9d865b022f5b7
LoylP <nhphuc183@gmail.com>
2024-09-10 12:04:05 +0700

    add logo movie-app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,15 +12,19 @@ export default function Header() {
             <MenuItem title='home' address='/' Icon={AiFillHome} />
             <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill} />
           </div>
-          {/* <div className='flex items-center gap-4'>
-            <DarkModeSwitch />
+          <div className='flex items-center gap-4'>
+            {/* <DarkModeSwitch /> */}
             <Link href={'/'} className='flex gap-1 items-center'>
-              <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
-                IMDb
-              </span>
-              <span className='text-xl hidden sm:inline'>Clone</span>
+            <div className='font-bold bg-blue-400 rounded-lg flex'>
+                <span className='text-green-600 font-bold text-3xl italic transform -skew-x-10 inline-block'>
+                    M
+                </span>
+                <span className='text-xl py-1'>
+                    Film
+                </span>
+            </div>
             </Link>
-          </div> */}
+          </div>
         </div>
     )
 }
