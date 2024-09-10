85ce36ffa4cf1eee26219d0cf33dcc9301fbc514
LoylP <nhphuc183@gmail.com>
2024-09-10 17:07:22 +0700

    Loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
+import Loading from '@/components/Loading';
 
 interface MediaItem {
   id: number;
@@ -18,9 +19,12 @@ export default function Home() {
   const [mediaType, setMediaType] = useState<'movie' | 'tv'>('movie');
   const searchParams = useSearchParams();
   const genre = searchParams.get('genre');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [initialLoad, setInitialLoad] = useState(true);
 
   useEffect(() => {
     const fetchMedia = async () => {
+      setLoading(true);
       try {
         let url = '';
         if (genre === 'fetchTrending') {
@@ -42,13 +46,21 @@ export default function Home() {
         }
         const data = await response.json();
         setMediaItems(data.results);
+        
       } catch (error) {
         console.error('Error fetching media:', error);
+      } finally {
+        setLoading(false);
+        setInitialLoad(false);
       }
     };
 
     fetchMedia();
   }, [mediaType, genre]);
+
+  if (initialLoad) {
+    return <Loading />;
+  }
 
   return (
     <div className="container mx-auto px-4">
@@ -70,19 +82,23 @@ export default function Home() {
         </button>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {mediaItems.map((item) => (
-          <div key={item.id} className="border rounded-lg overflow-hidden shadow-lg">
-            <img
-              src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-              alt={item.title || item.name}
-              className="w-full h-64 object-cover"
-            />
-            <div className="p-4">
-              <h2 className="font-bold text-xl mb-2">{item.title || item.name}</h2>
-              <p className="text-gray-700 text-base">{item.overview.slice(0, 100)}...</p>
+        {loading ? (
+          <Loading />
+        ) : (
+          mediaItems.map((item) => (
+            <div key={item.id} className="border rounded-lg overflow-hidden shadow-lg">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                alt={item.title || item.name}
+                className="w-full h-64 object-cover"
+              />
+              <div className="p-4">
+                <h2 className="font-bold text-xl mb-2">{item.title || item.name}</h2>
+                <p className="text-gray-700 text-base">{item.overview.slice(0, 100)}...</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
