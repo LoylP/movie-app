6118e5bd332631dcdc56de20f0dac3f48444da21
LoylP <nhphuc183@gmail.com>
2024-09-11 14:06:38 +0700

    return original

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { FaRegImage, FaPlay } from "react-icons/fa";
-// import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 interface CardProps {
   result: {
@@ -14,17 +14,17 @@ interface CardProps {
 }
 
 export default function Card({ result }: CardProps) {
-  // const router = useRouter();
+  const router = useRouter();
   const imagePath = result.backdrop_path || result.poster_path;
 
-  // const handleClick = () => {
-  //   localStorage.setItem('movieData', JSON.stringify(result));
-  //   router.push(`/movie/${result.id}`);
-  // };
+  const handleClick = () => {
+    localStorage.setItem("movieData", JSON.stringify(result));
+    router.push(`/movie/${result.id}`);
+  };
 
   return (
     <div className="relative border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-125 transform z-10 hover:z-50 group">
-      <div className="cursor-pointer">
+      <div onClick={handleClick} className="cursor-pointer">
         <div className="relative w-full pb-[70%]">
           {imagePath ? (
             <>
