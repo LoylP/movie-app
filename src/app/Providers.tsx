508c75dcc39cdd9b64f0ab329601f8954e54cfe5
LoylP <nhphuc183@gmail.com>
2024-09-27 01:31:06 +0700

    change background color

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react"
 export default function Providers({children} : {children: ReactNode}) {
     return(
         <ThemeProvider defaultTheme="system" attribute="class">
-            <div className='text-gray-700 dark:text-gray-200 dark:bg-slate-900 min-h-screen select-none transition-colors duration-300'>
+            <div className='text-gray-700 dark:text-gray-200 dark:bg-zinc-900 min-h-screen select-none transition-colors duration-300'>
                 {children}
             </div>
         </ThemeProvider>
